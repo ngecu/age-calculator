1587484c41b5741b0821f1a46e36800476d1bb84
ngecu <devngecu@gmail.com>
2023-10-17 16:06:04 +0300

    done

--- main.js
+++ main.js
@@ -1,126 +1,85 @@
-
-function dayValidation(dayGiven) {
-    if(dayGiven.trim().length == 0){
-        document.querySelector('.day_error').innerText = "This field is required";
-        const label = document.getElementById('day_label');
-        const input = document.getElementById('day');
-        label.style.color = 'red';
-        input.style.border = 'solid red';
-        return false
+document.addEventListener('DOMContentLoaded', function() {
+    // fetching elements by ID
+    const dayInput = document.getElementById("day");
+    const monthInput = document.getElementById("month");
+    const yearInput = document.getElementById("year");
+    const dayOutput = document.getElementById("DD");
+    const monthOutput = document.getElementById("MM");
+    const yearOutput = document.getElementById("YY");
+  
+    const form = document.querySelector(".form");
+  
+    // event listener for the form
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+  
+      // Clear previous errors
+      const errorMessages = document.querySelectorAll("small");
+      errorMessages.forEach(error => error.textContent = "");
+  
+      //check if date provided is valid
+      const isValidDate = (d, m, y) => {
+        const date = new Date(y, m - 1, d);
+        return date.getDate() == d && date.getMonth() + 1 == m && date.getFullYear() == y;
+      };
+       //values for the date
+      const inputDay = parseInt(dayInput.value);
+      const inputMonth = parseInt(monthInput.value);
+      const inputYear = parseInt(yearInput.value);
+  
+      //checking for empty fields
+      if (!inputDay || !inputMonth || !inputYear) {
+        dayInput.nextElementSibling.textContent = inputDay ? "" : "Day is required";
+        monthInput.nextElementSibling.textContent = inputMonth ? "" : "Month is required";
+        yearInput.nextElementSibling.textContent = inputYear ? "" : "Year is required";
+  
+        //day validity
+      } else if (inputDay < 1 || inputDay > 31) {
+        dayInput.nextElementSibling.textContent = "Invalid day";
+      
+        //month validity
+      } else if (inputMonth < 1 || inputMonth > 12) {
+        monthInput.nextElementSibling.textContent = "Invalid month";
+       
+        //year validity
+      } else if (inputYear > new Date().getFullYear()) {
+        yearInput.nextElementSibling.textContent = "Year can't be in the future";
+  
+        // whole date validity
+      } else if (!isValidDate(inputDay, inputMonth, inputYear)) {
+        dayInput.nextElementSibling.textContent = "Invalid date";
+      //if all validations pass calculate current age
+      } else {
+        //today's date
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = currentDate.getMonth() + 1;
+        const currentDay = currentDate.getDate();
+  
+        // Calculating THE AGE
+        let years = currentYear - inputYear;
+        let months = currentMonth - inputMonth;
+        let days = currentDay - inputDay;
+  
+        // Displaying the age with animation
+        animateValue(yearOutput, 0, years, 2000);
+        animateValue(monthOutput, 0, months, 2000);
+        animateValue(dayOutput, 0, days, 2000);
+      }
+    });
+  
+    // Animate the numbers
+    function animateValue(element, start, end, duration) {
+      let current = start;
+      let range = end - start;
+      let increment = end > start ? 1 : -1;
+      let stepTime = Math.abs(Math.floor(duration / range));
+      let timer = setInterval(function() {
+        current += increment;
+        element.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, stepTime);
     }
-    if (dayGiven < 1 || dayGiven > 31) {
-        document.querySelector('.day_error').innerText = "Must be a valid day";
-        const label = document.getElementById('day_label');
-        const input = document.getElementById('day');
-        label.style.color = 'red';
-        input.style.border = 'solid red';
-        return false
-    }
-    else{
-        return true
-    }
-    
-}
-
-function monthValidation(monthGiven) {
-    if(monthGiven.trim().length == 0){
-        document.querySelector('.month_error').innerText = "This field is required";
-        const label = document.getElementById('month_label');
-        const input = document.getElementById('month');
-        label.style.color = 'red';
-        input.style.border = 'solid red';
-        return false
-    }
-    if (monthGiven < 1 || monthGiven > 12) {
-        document.querySelector('.month_error').innerText = "Must be a valid month";
-        const label = document.getElementById('month_label');
-        const input = document.getElementById('month');
-        label.style.color = 'red';
-        input.style.border = 'solid red';
-        
-        return false
-
-    }
-    else{
-        return true
-    }
-    
-}
-
-document.getElementById('ageForm').addEventListener('submit', function(event) {
-    const daydate = document.getElementById('day').value;
-const monthdate = document.getElementById('month').value;
-const yeardate = document.getElementById('year').value;
-
-clearValidationError()
-    event.preventDefault();
-    
-
-    // const birthdate = document.getElementById('birthdate').value;
-    if (dayValidation(daydate)) {
-        if (monthValidation(monthdate)) {
-            
-        
-        
-    const birthDate = new Date(`${yeardate}-${monthdate}-${daydate}`);
-
-
-    const today = new Date();
-
-    console.log(birthDate)
-
-    if (!isValidDate(birthDate) || birthDate > today) {
-        displayValidationError("Invalid Date.");
-        return;
-    }
-
-    const ageInMilliseconds = today - birthDate;
-    const ageInYears = Math.floor(ageInMilliseconds / (365 * 24 * 60 * 60 * 1000));
-    const ageInMonths = Math.floor(ageInMilliseconds / (30 * 24 * 60 * 60 * 1000));
-    const ageInDays = Math.floor(ageInMilliseconds / (24 * 60 * 60 * 1000));
-
-    document.querySelector('.year_span').innerText = ageInYears
-    document.querySelector('.month_span').innerText = ageInMonths
-    document.querySelector('.day_span').innerText = ageInDays
-    
-    }    
-}
-
-});
-
-
-function isValidDate(date) {
-    return date instanceof Date && !isNaN(date);
-}
-
-function displayValidationError(message) {
-    document.querySelector('.year_error').innerText = message;
-
-    const label = document.getElementById('year_label');
-    const input = document.getElementById('year');
-    label.style.color = 'red';
-    input.style.border = 'solid red';
-
-}
-
-function clearValidationError() {
-    const day_label = document.getElementById('day_label');
-    const day_input = document.getElementById('day');
-    day_label.style.color = '#717171';
-    day_input.style.border = '2px solid #DDDDDD'; 
-    
-    const month_label = document.getElementById('month_label');
-    const month_input = document.getElementById('month');
-    month_label.style.color = '#717171';
-    month_input.style.border = '2px solid #DDDDDD'; 
-    
-    const year_label = document.getElementById('year_label');
-    const year_input = document.getElementById('year');
-    year_label.style.color = '#717171';
-    year_input.style.border = '2px solid #DDDDDD'; 
-    
-
-    document.querySelector('.year_error').innerText = "";
-    document.querySelector('.month_error').innerText = "";
-    document.querySelector('.day_error').innerText = "";
-}
+  });
